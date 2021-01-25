cfe19aaef1e511f2914ea4d47af644599374cda6
Xrunov <vovkavovka8@gmail.com>
2021-01-25 18:10:01 +0300

    Updates

--- src/configurations/PenyConfig.js
+++
@@ -1,7 +0,0 @@
-const PenyConfig = {
-  refinanceRate: 1 / 300,
-  reqLink: "https://shielded-oasis-30862.herokuapp.com/api/cbr/"
-};
-Object.preventExtensions(PenyConfig);
-
-export default PenyConfig;
